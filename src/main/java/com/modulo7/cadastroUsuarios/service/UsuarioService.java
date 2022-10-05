daa7f6256e72f857a26dbce85fa69fb19ad0d401
davispacheco <davipacheco1996@gmail.com>
2022-10-05 15:20:40 -0300

    Adicionar conteúdo à classe UsuarioController.

--- src/main/java/com/modulo7/cadastroUsuarios/service/UsuarioService.java
+++ src/main/java/com/modulo7/cadastroUsuarios/service/UsuarioService.java
@@ -1,11 +1,38 @@
 package com.modulo7.cadastroUsuarios.service;
 
+import com.modulo7.cadastroUsuarios.DTO.UsuarioDTO;
+import com.modulo7.cadastroUsuarios.DTO.UsuarioRespostaDTO;
+import com.modulo7.cadastroUsuarios.model.UsuarioModel;
 import com.modulo7.cadastroUsuarios.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    public List<UsuarioRespostaDTO> buscarTodos() {
+        List<UsuarioModel> usuarios = usuarioRepository.findAll();
+        return UsuarioRespostaDTO.converterLista(usuarios);
+    }
+
+    public Optional<UsuarioModel> buscarPorId(Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public UsuarioModel cadastrar(UsuarioModel usuarioModel) {
+        return usuarioRepository.save(usuarioModel);
+    }
+
+    public UsuarioModel alterar(UsuarioModel usuarioModel) {
+        usuarioRepository.save(usuarioModel);
+    }
+
+    public void deletar(Long id) {
+        usuarioRepository.deleteById(id);
+    }
 }
